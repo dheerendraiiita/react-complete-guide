d0f62291d47ece804de0b564df288194ffb0bc82
Dheerendra Kumar <dheerendra.iiit@gmail.com>
2018-06-06 10:02:55 +0530

    working on actions, reducers, store

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routers from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './styles/styles.css'
+import './styles/styles.css';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
+
 render(
-  <Router history={browserHistory} routes={routers} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routers} />
+  </Provider>,
   document.getElementById('app')
 );
