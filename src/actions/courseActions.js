5a21fb519463a7ce7ece8e15d8d3917516f81a68
Dheerendra Kumar <dheerendra.iiit@gmail.com>
2018-06-07 08:42:07 +0530

    added Mock API

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,24 @@
+import courseApi from './../api/mockCourseApi';
+
+import * as types from './actionTypes';
+
 export function createCourse(course) {
   return {
-    type: 'CREATE_COURSE',
+    type: types.CREATE_COURSE,
     course
+  };
+}
+
+export function  loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    })
+      .catch(error => {
+        throw error;
+      });
   };
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,10 @@
+import * as types from './../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.LOAD_COURSES_SUCCESS:
       //state.push(action.course);
-      return [...state,
-        Object.assign({}, action.course)
-        ];
+      return action.courses;
 
     default:
       return state;
